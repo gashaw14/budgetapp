0a94b96edddb715aedf6379a6bdc86f9ed30b474
gashaw <gashawb14@gmail.com>
2020-10-23 21:58:41 -0500

    filter reducer

--- src/test/selectors/expenses.test.js
+++ src/test/selectors/expenses.test.js
@@ -46,3 +46,35 @@ test('should filter by start date',()=>{
     expect(action).toEqual([expenses[1],expenses[0]]);
 });
 
+test('should filter by end date',()=>{
+    const filters ={
+        text:'',
+        sortBy:'date',
+        startDate: undefined,
+        endDate: moment(0)
+    };
+    const action = getVisibileExpenses(expenses,filters);
+    expect(action).toEqual([expenses[0],expenses[2]]);
+});
+
+test('should filter by date',()=>{
+    const filters ={
+        text:'',
+        sortBy:'date',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const action = getVisibileExpenses(expenses,filters);
+    expect(action).toEqual([expenses[1],expenses[0],expenses[2]]);
+});
+
+test('should filter by amount',()=>{
+    const filters ={
+        text:'',
+        sortBy:'amount',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const action = getVisibileExpenses(expenses,filters);
+    expect(action).toEqual([expenses[1],expenses[2],expenses[0]]);
+})
