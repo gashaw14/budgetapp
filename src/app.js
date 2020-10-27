e657479b2bb610128c0d42694de56c8bdefd1265
gashaw <gashawb14@gmail.com>
2020-10-26 20:43:02 -0500

    update style

--- src/app.js
+++ src/app.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './router/AppRouter';
 import {ConfigStore} from './store/configStore';
-import {getVisibileExpenses} from './selectors/expenses';
-import {addExpense} from './actions/expenses';
 import { Provider } from 'react-redux';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+
 
 const store = ConfigStore();
-const expenseOne = store.dispatch(addExpense({description:'rent 1st payment', amount:5000000, createdAt:-2100}))
-const expenseTwo = store.dispatch(addExpense({description:'lunch expense', amount:50000, createdAt:-1000}))
 
-const state= store.getState()
-const visibleExpenses = getVisibileExpenses(state.expenses,state.filters)
-console.log(visibleExpenses)
-
+// const state= store.getState()
+// const visibleExpenses = getVisibileExpenses(state.expenses,state.filters)
+// console.log(visibleExpenses)
 
 const jsx = (
     <Provider store={store}>
